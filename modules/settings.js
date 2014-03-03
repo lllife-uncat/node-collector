1163e6bc738ddf0d951147f2d76a18418c21db1e
Somnuk Wongkhan <somnuk.wk@gmail.com>
2014-03-03 11:01:58 +0700

    test connection

--- modules/settings.js
+++ modules/settings.js
@@ -2,18 +2,14 @@
 var dev = {
     connectionString : "localhost:27017/collector",
     ipAddress: "0.0.0.0",
-    port: 8080,
-    touchsPath : "/touchs",
-    pirsPath : "/pirs"
+    port: 8080
 };
 
 var production = {
-    connectionString : "mongodb://admin:yYsEAGxCIDn4@$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/collector",
+    connectionString : "mongodb://admin:yYsEAGxCIDn4@127.4.69.2:27017",
     ipAddress: "0.0.0.0",
-    port: 8080,
-    touchsPath : "/touchs",
-    pirsPath : "/pirs"
+    port: 8080
 };
 
 exports.configs = dev;
-//exports.configs = production;
+exports.configs = production;
